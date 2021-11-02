63991584dfee0bcc5abd5efa58cec4e2f7483118
pedroisb <pedroisb28@gmail.com>
2021-11-02 02:55:38 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.fOvYJq1cQda970efdW58mgHaE7%26pid%3DApi&f=1" alt="homem feliz"/>
+    </div>
+  );
+}
+
+export default App;
